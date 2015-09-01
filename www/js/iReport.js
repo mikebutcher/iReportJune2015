e1fd11873a6565784b13abfaa7b98495e14bc41c
mikebutcher <mbutche1@gmail.com>
2015-08-31 22:21:35 -0400

    Update iReport.js

--- www/js/iReport.js
+++ www/js/iReport.js
@@ -290,7 +290,7 @@ function doDominoLoginSilent(username, password) {
 
     if (logReq.status == 200) {
 	//$("#progress").remove();
-	alert(logReq.status);
+//	alert(logReq.status);
         lastposition = logReq.responseText.lastIndexOf("###");
 
 
